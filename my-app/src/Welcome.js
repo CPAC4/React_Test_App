30566f2a1a1ba4ac18c54b65f72f5d75b2a0b86d
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-10-09 21:53:52 +0530

    tailwind configured

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -2,8 +2,11 @@ import React from 'react'
 import './index.css'
 export default function Welcome() {
     return (
-        <div style={{position: "absolute", "top":"25%", "left":"30%",}}>
-            <img src="/img/CPAC-NOW.png" />
+        // <div style={{position: "absolute", "top":"25%", "left":"30%",}}>
+        //     <img src="/img/CPAC-NOW.png" />
+        // </div>
+        <div className="w-full h-screen flex justify-center items-center">
+            <img className=" w-96 " src="/img/CPAC-NOW.png" />
         </div>
     )
 }
